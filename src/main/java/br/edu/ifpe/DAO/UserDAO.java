f7e596ac4e484ec910e77fecb7fd377c2d09eb8b
gabriel4g <gabr.lucena@hotmail.com>
2021-03-02 14:38:51 -0300

    DAO corrigido

--- src/main/java/br/edu/ifpe/DAO/UserDAO.java
+++ src/main/java/br/edu/ifpe/DAO/UserDAO.java
@@ -1,9 +1,8 @@
 package br.edu.ifpe.DAO;
 
-import br.edu.ifpe.models.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.edu.ifpe.models.User;
 
 
-public interface UserDAO extends JpaRepository<Produto, Integer> {
+public interface UserDAO extends JpaRepository<User, Integer> {
 }
