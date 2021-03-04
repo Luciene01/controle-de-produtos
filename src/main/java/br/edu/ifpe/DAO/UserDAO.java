5414c7d9f405035467a7209e760111415485b2fb
gabriel4g <gabr.lucena@hotmail.com>
2021-03-04 12:27:10 -0300

    renomeado

--- src/main/java/br/edu/ifpe/DAO/UserDAO.java
+++
@@ -1,8 +0,0 @@
-package br.edu.ifpe.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.edu.ifpe.models.User;
-
-
-public interface UserDAO extends JpaRepository<User, Integer> {
-}
