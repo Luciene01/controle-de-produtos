69fd59f0719bfc449b807a65af2a372be88628c8
gabriel4g <gabr.lucena@hotmail.com>
2021-03-11 21:21:11 -0300

    atualizado

--- src/main/java/br/edu/ifpe/DAO/UsuarioDAO.java
+++ src/main/java/br/edu/ifpe/DAO/UsuarioDAO.java
@@ -1,8 +1,12 @@
 package br.edu.ifpe.DAO;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import br.edu.ifpe.models.Usuario;
 
-
 public interface UsuarioDAO extends JpaRepository<Usuario, Integer> {
+
+
+	Usuario findByEmailAndSenha(String email, String senha);
+
 }
